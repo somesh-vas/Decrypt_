72cb32be7f0302a27709d3da7fbb1f9fa24cafb3
somesh-vas <someshvas1@gmail.com>
2025-06-11 21:16:49 +0530

    BM_DECRYPT

---
+++ BM_Decrypt/BM_Decrypt_single/gf.h
@@ -0,0 +1,47 @@
+
+// #ifndef GF_H
+// #define GF_H
+// #include "common.h"
+
+
+// gf bitrev(gf a);
+// uint16_t load_gf(const unsigned char *src);
+// void store_gf(unsigned char *dest, gf a);
+// uint32_t load4(const unsigned char * in);
+// void store8(unsigned char *out, uint64_t in);
+// uint64_t load8(const unsigned char * in);
+// gf gf_mul(gf a, gf b);
+// gf gf_frac(gf a, gf b);
+// gf gf_inv(gf a);
+// gf gf_add(gf a, gf b);
+// gf gf_iszero(gf a);
+
+
+
+// #endif // !GF_H
+#ifndef GF_H
+#define GF_H
+
+#include "common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+gf bitrev(gf a);
+uint16_t load_gf(const unsigned char *src);
+void store_gf(unsigned char *dest, gf a);
+uint32_t load4(const unsigned char * in);
+void store8(unsigned char *out, uint64_t in);
+uint64_t load8(const unsigned char * in);
+gf gf_mul(gf a, gf b);
+gf gf_frac(gf a, gf b);
+gf gf_inv(gf a);  // Prevent name mangling
+gf gf_add(gf a, gf b);
+gf gf_iszero(gf a);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !GF_H
